e4c1deb097607623676998794e172f178a83c4a6
Sanket Naik <sanketnaik99@gmail.com>
2023-05-30 09:05:38 -0700

    Adds debug logging

--- notes.js
+++ notes.js
@@ -51,7 +51,7 @@ async function changesByLabel(commitMessages) {
   let headingLabels = core.getInput('labels').split(',')
   let headingTitles = core.getInput('titles').split(',')
   let commitMessageFilters = core.getInput('filters').split(',')
-
+  console.log('Filters', commitMessageFilters)
   if (headingLabels.length !== headingTitles.length) {
     throw new Error('The number of labels and titles do not match')
   }
@@ -61,6 +61,7 @@ async function changesByLabel(commitMessages) {
     if (commitMessageFilters.length > 0) {
       for (const filter of commitMessageFilters){
         if (commitMsg.startsWith(filter)){
+          console.log('Found match', commitMsg, filter)
           filteredCommitMessage = commitMsg.replace(filter, '')
         }
       }
