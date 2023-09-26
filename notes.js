7334549e4481ce2e15172e4eb099615455dfd50d
Sanket Naik <sanketnaik99@gmail.com>
2023-09-26 09:42:19 -0500

    Adds log for contains

--- notes.js
+++ notes.js
@@ -59,11 +59,14 @@ async function changesByLabel(commitMessages) {
     var added = false
     if (commitMessageFilters.length > 0) {
       for (const filter of commitMessageFilters){
-        console.log({'filter': filter, 'commitMessage': commitMsg});
+        // console.log({'filter': filter, 'commitMessage': commitMsg});
         if (commitMsg.startsWith(filter)){
-          console.log(commitMsg, " starts with", filter);
+          // console.log(commitMsg, " starts with", filter);
           filteredCommitMessage = commitMsg.replace(filter, '')
           console.log("filteredCommitMessage: ", filteredCommitMessage)
+        }
+        if (commitMsg.contains(filter)){
+          console.log(commitMsg, " contains", filter);
         }
       }
     }
